747bca0b7a787586c93ac82ada860c7e9b90da95
Reza PF <rezafebriyan@gmail.com>
2023-06-20 16:05:12 +0700

    fix: validation for users

--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -1,9 +1,9 @@
-import Joi from ("joi")
+import Joi from "joi"
 
 const registerUserValidation = Joi.object({
     username: Joi.string().max(50).required(),
     password: Joi.string().max(100).required(),
-    name: Joi.string.max(50).required()
+    name: Joi.string().max(50).required()
 })
 
 export { registerUserValidation }
--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,8 +1,10 @@
+import { ResponseError } from "../error/responseError.js"
+
 const validate = (schema, request) => {
     const result = schema.validate(request)
 
     if(result.error) {
-        throw result.error
+        throw new ResponseError(400, result.error.message)
     }
     return result.value
 }

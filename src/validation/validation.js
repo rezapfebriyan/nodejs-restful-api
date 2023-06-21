779d2c4edf50919fba881056a4e43799abed7937
Reza PF <rezafebriyan@gmail.com>
2023-06-21 16:31:04 +0700

    update set up validation

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,7 +1,9 @@
 import { ResponseError } from "../error/responseError.js"
 
 const validate = (schema, request) => {
-    const result = schema.validate(request)
+    const result = schema.validate(request, {
+        abortEarly: false // validate all request, not only one
+    })
 
     if(result.error) {
         throw new ResponseError(400, result.error.message)

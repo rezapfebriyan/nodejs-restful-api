eb1e46c9c4f329e0690ccc36d2103424935815f7
Reza PF <rezafebriyan@gmail.com>
2023-06-22 16:37:44 +0700

    create API get user authentication

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,6 +1,6 @@
 import { prismaClient } from "../application/database.js"
 import { ResponseError } from "../error/responseError.js"
-import { loginUserValidation, registerUserValidation } from "../validation/userValidation.js"
+import { getUserValidation, loginUserValidation, registerUserValidation } from "../validation/userValidation.js"
 import { validate } from "../validation/validation.js"
 import bcrypt from "bcrypt"
 import { v4 as uuid } from "uuid"
@@ -67,4 +67,24 @@ const login = async (request) => {
     })
 }
 
-export default { register, login }
+const get = async (username) => {
+    username = validate(getUserValidation, username)
+
+    const user = await prismaClient.user.findUnique({
+        where: {
+            username: username
+        },
+        select: {
+            username: true,
+            name: true
+        }
+    })
+
+    if (!user) {
+        throw new ResponseError(404, "User is not found")
+    }
+
+    return user
+}
+
+export default { register, login, get }

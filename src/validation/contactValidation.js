fd8159f74092313437cb5f6d08a42223f8a9c6f8
rezapfebriyan <rezafebriyan00@gmail.com>
2023-07-05 16:18:34 +0700

    validation API update contact

--- src/validation/contactValidation.js
+++ src/validation/contactValidation.js
@@ -9,4 +9,12 @@ const createContactValidation = Joi.object({
 
 const getContactValidation = Joi.number().positive().required()
 
-export { createContactValidation, getContactValidation }
+const updateContactValidation = Joi.object({
+    id: Joi.number().positive().required(),
+    first_name: Joi.string().max(50).required(),
+    last_name: Joi.string().max(50).optional(),
+    email: Joi.string().max(100).email().optional(),
+    phone: Joi.string().max(15).optional()
+})
+
+export { createContactValidation, getContactValidation, updateContactValidation }

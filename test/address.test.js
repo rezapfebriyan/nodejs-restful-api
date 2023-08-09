01a253ece16e9c02b1abbb0a737cb5724c6e0755
rezapfebriyan <rezafebriyan00@gmail.com>
2023-08-09 14:20:22 +0700

    integration test: API update contact address

--- test/address.test.js
+++ test/address.test.js
@@ -119,3 +119,77 @@ describe('Get address contact', function () {
         expect(result.body.errors).toBeDefined()
     });
 })
+
+describe('Update contact address', function () {
+    beforeEach(async () => {
+        await createTestUser()
+        await createTestContact()
+        await createTestAddress()
+    })
+
+    afterEach(async () => {
+        await removeAllTestAddresses()
+        await removeAllTestContact()
+        await removeTestUser()
+    })
+
+    it('should can update address', async () => {
+        const testContact = await getTestContact()
+        const testAddress = await getTestAddress()
+
+        const result = await supertest(web)
+            .put('/api/contacts/' + testContact.id + '/addresses/' + testAddress.id)
+            .set('Authorization', 'test')
+            .send({
+                street: "update jalan test",
+                city: "test update city",
+                province: "test update province",
+                country: "indonesia",
+                postal_code: "131928"
+            })
+
+        expect(result.status).toBe(200)
+        expect(result.body.data.id).toBe(testAddress.id)
+        expect(result.body.data.street).toBe("update jalan test")
+        expect(result.body.data.city).toBe("test update city")
+        expect(result.body.data.province).toBe("test update province")
+        expect(result.body.data.country).toBe("indonesia")
+        expect(result.body.data.postal_code).toBe("131928")
+    })
+
+    it('should reject if request is not valid', async () => {
+        const testContact = await getTestContact()
+        const testAddress = await getTestAddress()
+
+        const result = await supertest(web)
+            .put('/api/contacts/' + testContact.id + '/addresses/' + testAddress.id)
+            .set('Authorization', 'test')
+            .send({
+                street: "street",
+                city: 'city',
+                province: true,
+                country: '',
+                postal_code: 1928
+            })
+
+        expect(result.status).toBe(400)
+        expect(result.body.errors).toBeDefined()
+    })
+
+    it('should reject if address (or contact) is not found', async () => {
+
+        const result = await supertest(web)
+            .put('/api/contacts/99/addresses/99')
+            .set('Authorization', 'test')
+            .send({
+                street: "jalan test",
+                city: "test city",
+                province: "test province",
+                country: "indonesia",
+                postal_code: "1928"
+            })
+
+        expect(result.status).toBe(404)
+        expect(result.body.errors).toBeDefined()
+    })
+})

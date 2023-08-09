8f2da0c27096bc4b3b3113abc6a8702e61f6a56e
rezapfebriyan <rezafebriyan00@gmail.com>
2023-08-09 10:37:04 +0700

    test API add contact address

--- test/address.test.js
+++ test/address.test.js
@@ -1,5 +1,12 @@
 import supertest from "supertest"
-import { createTestContact, createTestUser, getTestContact, removeAllTestAddresses, removeAllTestContact, removeTestUser } from "./testUtil.js"
+import {
+    createTestContact,
+    createTestUser,
+    getTestContact,
+    removeAllTestAddresses,
+    removeAllTestContact,
+    removeTestUser
+} from "./testUtil.js"
 import { web } from "../src/application/web.js"
 
 describe('Create address', function () {
@@ -35,5 +42,23 @@ describe('Create address', function () {
         expect(result.body.data.province).toBe("test province")
         expect(result.body.data.country).toBe("indonesia")
         expect(result.body.data.postal_code).toBe("1928")
+    })
+
+    it('should reject if address request is invalid', async () => {
+        const testContact = await getTestContact()
+
+        const result = await supertest(web)
+            .post('/api/contacts/' + testContact.id + '/addresses')
+            .set('Authorization', 'test')
+            .send({
+                street: "jalan test",
+                city: "",
+                province: "test province",
+                country: "indonesia",
+                postal_code: 1928
+            })
+
+        expect(result.status).toBe(400)
+        expect(result.body.errors).toBeDefined()
     })
 })
